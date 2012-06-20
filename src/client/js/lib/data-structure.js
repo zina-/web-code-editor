ffb9955e329d2968771ee5519bc98997a8d677af
zina <127.0.0.1.zina@gmail.com>
2012-06-20 16:32:15 +0900

    prototype yet

--- src/client/js/lib/data-structure.js
+++ src/client/js/lib/data-structure.js
@@ -1,49 +1,69 @@
 /*
+**
+** THIS IS PROTOTYPE CODE
+**
+*/
+
+/*
 ** head - node - node - tail
 ** node
 **   a b c d e f g  - lineLength 7
 **  0 1 2 3 4 5 6 7 - charactorCaret
 **
 **
-** data structure class of code editor is based on editor provides that :
+** data structure class of code editor is based on that editor provides :
 ** - can get current line number
 ** - can get current caret position
 */
+var CarriageReturnCharactors = "\n";
 var EditableLinkedList = function () {
     this.head = new EditableNode();
     this.tail = new EditableNode();
     this.lineCaret = null;
 
     this.head.prev = null;
+    this.head.next = this.tail;
     this.tail.next = null;
+    this.tail.prev = this.head;
     this.head.lineNumber = 0;
     this.tail.lineNumber = 0;
 
     this.charactorCaret = 0;
-
-    this.tabCharactor = "    ";
+    this.listLength = 0;
+    this.tabCharactors = ["\t", "    "];
 
     this.init();
 };
 EditableLinkedList.prototype = {
     init: function () {
         var node = new EditableNode();
-        this.lineCaret = node;
 
-        this.head.next = node;
-        node.next = this.tail;
-        node.prev = this.head;
-        this.tail.prev = node;
+        this.addNode(node);
     },
-    lineScanner: function (start_line) {
+    doLineScan: function () {
+        var indexer = this.head.next;
+        var lineNumbering = 1;
+        while (indexer != this.tail) {
+            indexer.lineNumber = lineNumbering;
+            lineNumbering += 1;
+        }
+    },
+    tabfy: function (node) {
+
     },
     addNode: function (node) {
         this.tail.prev.next = node;
         node.prev = this.tail.prev;
         this.tail.prev = node;
         node.next = this.tail;
+
+        this.lineCaret = node;
+
+        this.listLength += 1;
+        node.lineNumber = this.listLength;
     },
     addNodeCurrentCaret: function (node) {
+        var node = new EditableNode();
     },
     moveUp: function () {
         if (this.lineCaret.prev != this.head) {
@@ -94,6 +114,16 @@ EditableLinkedList.prototype = {
             indexer = indexer.next;
         }
         return result;
+    },
+    toSourceString: function () {
+        var indexer = this.head.next;
+        var result = "";
+        while (indexer != this.tail) {
+            result += "" + indexer.lineNumber + ". " + indexer.line + CarriageReturnCharactors;
+            // todo : add the carriege return charactor
+            indexer = indexer.next;
+        }
+        return result;
     }
 };
 
@@ -106,10 +136,30 @@ var FunctionalParser = function () {
 var FunctionalScanner = function () {
 };
 
+// EditableNode object is make when
+// 1. proceed new line (press ENTER key)        --+-- same process
+// 1. splited line (press ENTER key in line)    --+
+// 1. pasted multi line
 var EditableNode = function (line) {
-    this.line = line || "";
+    //this.line = line || "";
+    this.setLine(line);
     this.lineLength = this.line.length;
     this.lineNumber = 0;
+    this.tabCount = 0;
 };
 EditableNode.prototype = {
+    setLine: function (line) {
+        if (line) {
+            line = line.split(CarriageReturnCharactors);
+            if (line.length > 1) {
+
+            }
+            else {
+                this.line = line[0];
+            }
+        }
+        else {
+            this.line = "";
+        }
+    }
 };
